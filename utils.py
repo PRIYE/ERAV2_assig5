734a760a6fbd1563a67e0c8c8529fe47f69be52b
PRIYE <priyeindia20@gmail.com>
2024-02-24 02:19:04 +0530

    added files

---
+++ utils.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+
+def plot_data(data):
+  batch_data, batch_label = next(iter(data))
+  fig = plt.figure()
+  for i in range(12):
+    plt.subplot(3,4,i+1)
+    plt.tight_layout()
+    plt.imshow(batch_data[i].squeeze(0), cmap='gray')
+    plt.title(batch_label[i].item())
+    plt.xticks([])
+    plt.yticks([])
+
+
+def GetCorrectPredCount(pPrediction, pLabels):
+  return pPrediction.argmax(dim=1).eq(pLabels).sum().item()
+
+def plot_performance(train_losses, train_acc, test_losses, test_acc):
+  fig, axs = plt.subplots(2,2,figsize=(15,10))
+  axs[0, 0].plot(train_losses)
+  axs[0, 0].set_title("Training Loss")
+  axs[1, 0].plot(train_acc)
+  axs[1, 0].set_title("Training Accuracy")
+  axs[0, 1].plot(test_losses)
+  axs[0, 1].set_title("Test Loss")
+  axs[1, 1].plot(test_acc)
+  axs[1, 1].set_title("Test Accuracy")
+
+
+
